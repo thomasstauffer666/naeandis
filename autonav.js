ce6246f88028a360ba976740c40c432397310dd7
Thomas Stauffer <thomasstauffer@gmx.ch>
2024-01-22 19:03:33 +0100

    Inspirationskarten & Anrede & Hilfreich/Wichtig

--- autonav.js
+++ autonav.js
@@ -2,8 +2,12 @@ function autonav() {
 	const root = document.createElement('ul');
 	var parent = root;
 	var levelPrevious = 1;
-	document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(h => {
-		const levelCurrent = parseInt(h.tagName.substring(1));
+	const nav = document.getElementsByTagName('nav')[0];
+	if (nav === undefined) {
+		return;
+	}
+	document.querySelectorAll('h2, h3, h4, h5, h6').forEach(h => {
+		const levelCurrent = parseInt(h.tagName.substring(1)) - 1;
 		//const id = 'nav-' + levelCurrent + '-' + h.textContent.toLowerCase();
 		const id = h.textContent;
 		h.setAttribute('id', id);
@@ -25,10 +29,7 @@ function autonav() {
 		parent.appendChild(li);
 		levelPrevious = levelCurrent;
 	});
-	const nav = document.getElementsByTagName('nav')[0];
-	if (nav !== undefined) {
-		nav.appendChild(root);
-	}
+	nav.appendChild(root);
 }
 
 document.addEventListener('DOMContentLoaded', autonav);
