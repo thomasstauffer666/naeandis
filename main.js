b920ca031d8404ef66566d79ab99cf11f6c7676c
Thomas Stauffer <thomasstauffer@gmx.ch>
2025-07-20 22:12:29 +0200

    Inhalt 2025-07

--- main.js
+++ main.js
@@ -13,15 +13,44 @@ function unique(arr) {
     return [...new Set(arr)];
 }
 
+function getWords(text) {
+    const re = /[\p{L}]+/gu;
+    return Array.from(text.matchAll(re), word => word[0]);
+}
+
+const strangeLetters = ['ß', '—', '–'];
+const strangeWords = ['man', 'jemand', 'niemand', 'freund', 'jedermann'];
+
 async function main() {
     const htmlFilePaths = await getHtmlFilePaths();
     for (const htmlFilePath of htmlFilePaths) {
         const html = await fs.readFile(htmlFilePath, 'utf-8');
         const dom = new jsdom.JSDOM(html);
+        const document = dom.window.document;
 
-        dom.window.document.querySelectorAll('[data-table]').forEach(node => {
+        document.querySelectorAll('[data-table]').forEach(node => {
             node.textContent = '\n' + unique(node.textContent.split(',').map($ => $.trim()).filter(Boolean)).sort((l, r) => r.localeCompare(l, 'de')).join(',\n') + '\n';
         });
+
+        const walker = document.createTreeWalker(document.body, dom.window.NodeFilter.SHOW_TEXT, null, false);
+
+        let node = null;
+        while (node = walker.nextNode()) {
+            const text = node.textContent.toLowerCase();
+            const words = getWords(text);
+
+            for (const strangeLetter of strangeLetters) {
+                if (text.includes(strangeLetter)) {
+                    console.log('StrangeLetter', htmlFilePath, strangeLetter);
+                }
+            }
+
+            for (const strangeWord of strangeWords) {
+                if (words.includes(strangeWord)) {
+                    console.log('StrangeWord', htmlFilePath, strangeWord);
+                }
+            }
+        }
 
         await fs.writeFile(htmlFilePath, dom.serialize(), 'utf-8');
     }
