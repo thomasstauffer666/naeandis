4b3d728c393d0f737ad27e574b8a8b97d3b97c92
Thomas Stauffer <thomasstauffer@gmx.ch>
2025-07-27 18:20:04 +0200

    I

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
 import fs from 'fs/promises';
 import path from 'path';
 import jsdom from 'jsdom';
+import sharp from 'sharp';
 
-async function getHtmlFilePaths() {
+async function getFilePaths(ext) {
     const dir = '.';
     const entries = await fs.readdir(dir, { withFileTypes: true });
-    return entries.filter($ => $.name.endsWith('.html')).map($ => path.join(dir, $.name));
+    return entries.filter($ => $.name.endsWith(ext)).map($ => path.join(dir, $.name));
 }
 
 function unique(arr) {
@@ -21,13 +22,14 @@ function getWords(text) {
 const strangeLetters = ['ß', '—', '–'];
 const strangeWords = ['man', 'jemand', 'niemand', 'freund', 'jedermann'];
 
-async function main() {
-    const htmlFilePaths = await getHtmlFilePaths();
+async function htmlAnalyze() {
+    const htmlFilePaths = await getFilePaths('.html');
     for (const htmlFilePath of htmlFilePaths) {
-        const html = await fs.readFile(htmlFilePath, 'utf-8');
-        const dom = new jsdom.JSDOM(html);
+        const htmlInput = await fs.readFile(htmlFilePath, 'utf-8');
+        const dom = new jsdom.JSDOM(htmlInput);
         const document = dom.window.document;
 
+        // sort all data-table entries
         document.querySelectorAll('[data-table]').forEach(node => {
             node.textContent = '\n' + unique(node.textContent.split(',').map($ => $.trim()).filter(Boolean)).sort((l, r) => r.localeCompare(l, 'de')).join(',\n') + '\n';
         });
@@ -52,8 +54,17 @@ async function main() {
             }
         }
 
-        await fs.writeFile(htmlFilePath, dom.serialize(), 'utf-8');
+        // to some degree also normalizes HTML
+        const htmlOutput = dom.serialize();
+
+        if (htmlInput != htmlOutput) {
+            await fs.writeFile(htmlFilePath, htmlOutput, 'utf-8');
+        }
     }
+}
+
+async function main() {
+    await htmlAnalyze();
 }
 
 await main();
